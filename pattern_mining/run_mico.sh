d024acfda731611bf3703e14d7b3a449239fc1b9
Peng Jiang <pengjiang030@gmail.com>
2021-07-19 15:05:40 -0500

    .

---
+++ pattern_mining/run_mico.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/mico.lg 0.001 4 4 1 0 > output/fsm5_mico_001_4_4.txt
+#./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/mico.lg 0.001 4 6 1 0 > output/fsm5_mico_001_4_6.txt
+#./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/mico.lg 0.001 4 8 1 0 > output/fsm5_mico_001_4_8.txt
+./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/mico.lg 0.001 4 10 1 0 > output/fsm5_mico_001_4_10.txt
+#./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/mico.lg 0.005 4 4 1 0 > output/fsm5_mico_005_4_4.txt
+#./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/mico.lg 0.005 4 6 1 0 > output/fsm5_mico_005_4_6.txt
+#./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/mico.lg 0.005 4 8 1 0 > output/fsm5_mico_005_4_8.txt
+./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/mico.lg 0.005 4 10 1 0 > output/fsm5_mico_005_4_10.txt
--- pattern_mining/run_uk.sh
+++ pattern_mining/run_uk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0001 1 2 1 0 > output/fsm5_uk_0001_1_2.txt
-./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0001 1 4 1 0 > output/fsm5_uk_0001_1_4.txt
-./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0005 1 2 1 0 > output/fsm5_uk_0005_1_2.txt
-./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0005 1 4 1 0 > output/fsm5_uk_0005_1_4.txt
+./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0001 2 2 1 0 > output/fsm5_uk_0001_2_2.txt
+./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0001 2 4 1 0 > output/fsm5_uk_0001_2_4.txt
+./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0005 2 2 1 0 > output/fsm5_uk_0005_2_2.txt
+./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0005 2 4 1 0 > output/fsm5_uk_0005_2_4.txt
