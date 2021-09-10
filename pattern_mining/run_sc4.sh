72d828527b09bf09fc13936395ac7ca3e48be88c
Peng Jiang <pengjiang030@gmail.com>
2021-09-10 11:05:49 -0500

    mico join3 is slow

---
+++ pattern_mining/run_sc4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./sc4_tv.exe /data/not_backed_up/shared/graphs/mico.lg 32 > output/mico_4_32.txt &
+./sc4_tv.exe /data/not_backed_up/shared/graphs/mico.lg 64 > output/mico_4_64.txt &
---
+++ pattern_mining/test/sc4_tv.cpp
@@ -0,0 +1,98 @@
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
+#include "pattern_mining/gmine.h"
+#include "util.h"
+typedef boost::multiprecision::cpp_dec_float_100 value_type;
+
+using namespace std;
+using namespace euler;
+using namespace euler::pattern_mining;
+
+typedef vector<pair<int, int>> pat_t;
+
+
+// citeseer 4,5,6,7  // sampling ratio: 2,3,4
+// mico 4
+int main(int argc, char* argv[]) {
+  // system("rm test_temp/*");
+
+  graph::Graph_CSR_CPU g;
+
+
+  g.read_graph(argv[1]);
+
+  auto pat2 = pattern_mining::PatListing::make_pattern(
+    pattern_mining::PatListing().pattern_listing(2));
+
+
+  double st2 = atof(argv[2]);
+
+  cout << "start matchings pat2: " << endl;
+  auto d2 = match(g, pat2, true, false, true);
+
+  cout << "start join for pat3: " << endl;
+  vector<SGList> sgls2 = { d2, d2 };
+
+  util::Timer match_time;
+  match_time.start();
+  auto H2 = build_tables(sgls2);
+
+  auto [d3, ess3] = join<true, true, false, false, 2, 3, 3>(g, H2, sgls2, true, default_sampler, -1, true);
+
+  match_time.stop();
+
+  cout << "join for pat3 time: " << match_time.get() << " sec" << endl;
+
+  size_t npat3 = d3.sgl->size();
+
+  cout << "num of size-3 patterns: " << npat3 << endl;
+
+
+  vector<SGList> sgls = { d3, d2 };
+
+  cout << "building tables..." << endl;
+  auto H = build_tables(sgls);
+  cout << "build table done" << endl;
+
+  Sampler *sm2;
+  if (st2 > 0)
+    sm2 = new ProportionalSampler({ st2, sqrt(st2) });
+  else sm2 = &default_sampler;
+
+  util::Timer t;
+  t.start();
+  auto [d_res, ess] = join<true, true, false, false, 2, 4, 3>(g, H, sgls, false, *sm2, -1, false, st2 > 0);
+  t.stop();
+
+  cout << "Time: " << t.get() << " sec, ";
+  if (d_res.sgl) {
+    if (st2 == 0) {
+      cout << "Num patterns: " << d_res.sgl->keys.size() << endl;
+
+      vector<double> counts(d_res.sgl->count.begin(), d_res.sgl->count.end());
+      sort(counts.begin(), counts.end(), std::greater<double>());
+
+      for (int i = 0; i < (50 > counts.size() ? counts.size() : 50); i++) {
+        cout << counts[i] << endl;
+      }
+    }
+    else {
+      cout << "Num patterns: " << ess.size() << endl;
+
+      vector<double> counts;
+
+      for (auto& [k, v] : ess) counts.push_back(v);
+
+      sort(counts.begin(), counts.end(), std::greater<double>());
+
+      for (int i = 0; i < (50 > counts.size() ? counts.size() : 50); i++) {
+        cout << counts[i] << endl;
+      }
+    }
+  }
+  else {
+    cout << "Num patterns: 0" << endl;
+  }
+
+  return 0;
+}
