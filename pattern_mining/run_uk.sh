1d7a4ec7525668e76e301653d0051bc6e4fde485
Peng Jiang <pengjiang030@gmail.com>
2022-01-13 17:14:03 -0600

    Delete run_uk.sh

--- pattern_mining/run_uk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0001 2 > output/fsm5_uk_0001_2.txt
-./fsm5_two_vertex.exe /data/not_backed_up/shared/gpm_data/uk-2005.lg 0.0005 2 > output/fsm5_uk_0005_2.txt
