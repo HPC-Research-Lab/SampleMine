7da4bd37e57e28e890abcf2af67fb04cd309e5c4
yihuawei <wwyihua@gmail.com>
2022-08-15 11:08:23 -0500

    update

---
+++ pattern_mining/PACT_Test/automine_fsm.cpp
@@ -0,0 +1,45 @@
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
+#include "pattern_mining/gmine.h"
+#include "util.h"
+typedef boost::multiprecision::cpp_dec_float_100 value_type;
+
+using namespace std;
+using namespace euler;
+using namespace euler::pattern_mining;
+
+typedef vector<pair<int, int>> pat_t;
+
+
+int main(int argc, char* argv[]) {
+  // system("rm test_temp/*");
+
+  graph::Graph_CSR_CPU g;
+
+
+  g.read_graph(argv[1]);
+
+  int pat_size = atoi(argv[2]);
+
+  auto pat2 = pattern_mining::PatListing::make_pattern(
+    pattern_mining::PatListing().pattern_listing(pat_size));
+
+  double thh = atof(argv[3]);
+
+  double sup = (size_t)round(thh * g.num_nodes());
+
+  cout << "support threshold: " << sup << endl;
+
+  cout << "start matching: " << endl;
+  util::Timer t;
+  t.start();
+  auto d2 = match(g, pat2, false, true, true, sup);
+  filter(d2, sup);
+  t.stop();
+
+  cout << "Time: " << t.get() << " sec, ";
+  cout << "num of frequent patterns: " << d2.sgl->size() << endl;
+
+
+  return 0;
+}
---
+++ pattern_mining/PACT_Test/automine_sc.cpp
@@ -0,0 +1,38 @@
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
+#include "pattern_mining/gmine.h"
+#include "util.h"
+typedef boost::multiprecision::cpp_dec_float_100 value_type;
+
+using namespace std;
+using namespace euler;
+using namespace euler::pattern_mining;
+
+typedef vector<pair<int, int>> pat_t;
+
+
+int main(int argc, char* argv[]) {
+  // system("rm test_temp/*");
+
+  graph::Graph_CSR_CPU g;
+
+
+  g.read_graph(argv[1]);
+
+  int pat_size = atoi(argv[2]);
+
+  auto pat2 = pattern_mining::PatListing::make_pattern(
+    pattern_mining::PatListing().pattern_listing(pat_size));
+
+  cout << "start matching: " << endl;
+  util::Timer t;
+  t.start();
+  auto d2 = match(g, pat2, true, false, true, -1, false, true);
+  t.stop();
+
+  cout << "Time: " << t.get() * 1.9 << " sec, ";
+  cout << "num of frequent patterns: " << d2.sgl->size() << endl;
+
+
+  return 0;
+}
